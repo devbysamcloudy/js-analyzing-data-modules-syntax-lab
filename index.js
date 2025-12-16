1825d7d1a7abf8fe99b440b1c224bb9ce9fcd0f2
devbysamcloudy <snganga685@gmail.com>
2025-12-16 20:43:21 +0300

    updated.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+function combineUsers(...arrays) {
+ 
+  const mergedUsers = [].concat(...arrays); 
+  
+  const mergeDate = new Date().toLocaleDateString();
 
-
+  return {
+    users: mergedUsers,
+    merge_date: mergeDate
+  };
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
